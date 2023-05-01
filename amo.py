e4a6abf4cf99b7f9434e23720a09ae3d45918e65
AKANKSHA MALVIYA <aakankshamalviya.amo@gmail.com>
2023-05-02 02:54:43 +0530

    Add files via upload

---
+++ amo.py
@@ -0,0 +1,48 @@
+import turtle
+t=turtle.Turtle()
+
+t.penup()
+t.goto(-30,50) #centering in the screen
+t.pendown()
+t.pensize(10)
+t.pencolor("blue")
+ 
+t.right(65)
+t.forward(100)
+ 
+t.setpos(-30,50)
+t.right(50)
+t.forward(100)
+ 
+t.penup()
+t.setpos(-50,-10)
+t.right(65)
+t.pendown()
+t.backward(50)
+
+#for m
+t=turtle.Turtle()
+t.penup()
+#draw straight line
+t.goto(-30,50) #centering in the screen
+t.pendown()
+t.pensize(10)
+t.pencolor("red")
+ 
+t.right(90)
+t.forward(150)
+ 
+t.goto(-30,50)
+t.goto(20,-20)
+t.goto(65,50)
+t.goto(65,-100)
+#.....draw letter O with python turtle........!
+import turtle
+t=turtle.Turtle()
+t.penup()
+t.goto(-30,50) #centering in the screen
+t.pendown()
+t.pensize(10)
+t.pencolor("magenta")
+t.circle(100,None,100)
+
